46e47fb02f452d55c68642c0a0e8c440c655a714
Rodrigo Montemayor <rodrigo.montemayor@enevasys.com>
2021-10-10 01:10:44 -0500

    WebDriverListener update

--- src/test/java/support/WebDriverListener.java
+++ src/test/java/support/WebDriverListener.java
@@ -1,4 +1,5 @@
 package support;
+
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -16,7 +17,7 @@ import com.qmetry.qaf.automation.core.ConfigurationManager;
 import com.qmetry.qaf.automation.ui.webdriver.CommandTracker;
 import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebDriver;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriverCommandAdapter;
-
+import com.qmetry.qaf.automation.util.PropertyUtil;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -28,28 +29,23 @@ import io.appium.java_client.AppiumDriver;
 /**
  * com.basis.listener.ErrorOverrideListener.java
  *
- * @author
- * 		shalin.s
+ * @author shalin.s
  */
 
 public class WebDriverListener extends QAFWebDriverCommandAdapter {
 	Log logger = LogFactory.getLog(getClass());
 
 	@Override
-	public void beforeCommand(QAFExtendedWebDriver driver,
-			CommandTracker commandTracker) {
+	public void beforeCommand(QAFExtendedWebDriver driver, CommandTracker commandTracker) {
 
 		super.beforeCommand(driver, commandTracker);
 		//logger.info("BEFORE COMMAND!");
 
 		String command = commandTracker.getCommand();
 		if (command.equalsIgnoreCase(DriverCommand.GET_CURRENT_WINDOW_HANDLE)
-				&& (ConfigurationManager.getBundle().getString("platform")
-						.equalsIgnoreCase("android")
-						|| ConfigurationManager.getBundle().getString("platform")
-								.equalsIgnoreCase("ios")
-						|| ConfigurationManager.getBundle().getString("platform")
-								.equalsIgnoreCase("windows"))) {
+				&& (ConfigurationManager.getBundle().getString("platform").equalsIgnoreCase("android")
+						|| ConfigurationManager.getBundle().getString("platform").equalsIgnoreCase("ios")
+						|| ConfigurationManager.getBundle().getString("platform").equalsIgnoreCase("windows"))) {
 			commandTracker.setResponce(new Response());
 		}
 
@@ -86,13 +82,55 @@ public class WebDriverListener extends QAFWebDriverCommandAdapter {
 		// 	capabilities.setCapability("chromeOptions", chromeOptions);
 		// }
 
-		/*DesiredCapabilities capabilities = (DesiredCapabilities) desiredCapabilities;
+		PropertyUtil prop = new PropertyUtil(
+			System.getProperty("application.properties.file", "resources/application.properties"));
+		String downloadsFolderBase = prop.getPropertyValue("downloads.folder.base");
+		String userDataDir = prop.getPropertyValue("user.data.dir");
+
+		logger.info("Downloads folder base: " + downloadsFolderBase);
+		logger.info("User data dir: " + userDataDir);
+
+		logger.info("support.WebDriverListener.beforeInitialize(): Configuring...");
+		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd_HHmmss");
+		Util.DOWNLOADS_FOLDER = downloadsFolderBase + sdf.format(new java.util.Date());
+		File dir = new File(Util.DOWNLOADS_FOLDER);
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+
+		DesiredCapabilities capabilities = (DesiredCapabilities) desiredCapabilities;
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--no-sandbox");//setExperimentalOption("mobileEmulation", mobileEmulation);
-        chromeOptions.addArguments("--disable-dev-shm-usage");
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--window-size=1920x1080");
-        capabilities.setCapability("chromeOptions", chromeOptions);*/
+
+		// Arguments for testing Conga Composer (remove issues with cross-origin
+		// iframes)
+		chromeOptions.addArguments("--disable-web-security");
+		chromeOptions.addArguments("--user-data-dir=" + userDataDir);
+		chromeOptions.addArguments("--disable-site-isolation-trials");
+		// chromeOptions.addArguments("--allow-running-insecure-content");
+		// chromeOptions.addArguments("--allow-file-access-from-files");
+
+		// Arguments for downloading and testing PDF contents
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("profile.default_content_settings.popups", 0);
+		chromePrefs.put("download.default_directory", Util.DOWNLOADS_FOLDER);
+		chromePrefs.put("download.prompt_for_download", false);
+		chromePrefs.put("download.directory_upgrade", true);
+		chromePrefs.put("plugins.always_open_pdf_externally", true);
+		chromeOptions.setExperimentalOption("prefs", chromePrefs);
+
+		/*
+		 * chromeOptions.addArguments("--no-sandbox");//setExperimentalOption(
+		 * "mobileEmulation", mobileEmulation);
+		 * chromeOptions.addArguments("--disable-dev-shm-usage");
+		 * chromeOptions.addArguments("--headless");
+		 * chromeOptions.addArguments("--window-size=1920x1080");
+		 */
+
+		// chromeOptions.addArguments("--disable-features=IsolateOrigins");
+		// chromeOptions.addArguments("--allow-running-insecure-content");
+		// chromeOptions.addArguments("--force-fieldtrials=SiteIsolationExtensions/Control");
+		// chromeOptions.addArguments("--ignore-certificate-errors");
+		capabilities.setCapability("chromeOptions", chromeOptions);
 
 		super.beforeInitialize(desiredCapabilities);
 

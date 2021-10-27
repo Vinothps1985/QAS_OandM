aa4c1e92ecdaf39897bacdbaf9930cfca6d42af6
Rodrigo Montemayor <rodrigo.montemayor@enevasys.com>
2021-10-27 01:30:38 -0500

    Code to prevent Salesforce 'ads' from disrupting the app launch process

--- src/test/java/steps/shareable/ShrdLaunchApp.java
+++ src/test/java/steps/shareable/ShrdLaunchApp.java
@@ -13,19 +13,42 @@ import java.io.IOException;
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import steps.common.*;
+
 /**
 * @author Rodrigo Montemayor
 */
-public class ShrdLaunchApp extends WebDriverTestCase {
-
+public class ShrdLaunchApp extends WebDriverTestCase{
 	@QAFTestStep(description = "ShrdLaunchApp {0}")
 	public void customShrdLaunchApp(Object appName) {
 		
 		$("common.activeTab").waitForPresent();
-		$("applauncher.div").waitForPresent();
-		$("applauncher.div").waitForEnabled();
-		CommonStep.click("applauncher.div");
-		$("applauncher.input.text").waitForPresent();
+
+		//Sometimes the page displays some 'ads' about new features and disrupts
+		//the app launch process. We use several attempts for this
+		int maxAttempts = 5;
+		int attempt = 0;
+		boolean found = false;
+		while (attempt < maxAttempts) {
+			$("applauncher.div").waitForPresent();
+			$("applauncher.div").waitForEnabled();
+			CommonStep.click("applauncher.div");
+			try {
+				if (!$("applauncher.input.text").isPresent() || !$("applauncher.input.text").isEnabled()) {
+					$("applauncher.input.text").waitForPresent(10*1000); //Wait for 10 secs
+				}
+				if ($("applauncher.input.text").isPresent() && $("applauncher.input.text").isEnabled()) {
+					found = true;
+					break;
+				}
+			} catch (Exception x) {
+				//Do nothing
+			}
+			attempt++;
+		}
+		if (!found) {
+			throw new org.openqa.selenium.TimeoutException("Timed out waiting for applauncher.input.text to be present (" + maxAttempts + " attempts)");
+		}
+		
 		CommonStep.clear("applauncher.input.text");
 		CommonStep.sendKeys(""+String.valueOf(appName)+"","applauncher.input.text");
 		$("applauncher.link.main").waitForPresent();
@@ -34,7 +57,3 @@ public class ShrdLaunchApp extends WebDriverTestCase {
 		$("common.activeTab").waitForPresent();
 	}
 }
-
-
-
-
